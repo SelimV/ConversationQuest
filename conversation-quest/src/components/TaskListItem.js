3ab950a6f45e74db0daf82aded3eb69f62bbbf76
SelimV <selim.virtanen@gmail.com>
2022-11-05 22:05:52 +0200

    Reflection slider

--- conversation-quest/src/components/TaskListItem.js
+++ conversation-quest/src/components/TaskListItem.js
@@ -2,17 +2,24 @@ import {ListItem } from "@mui/material";
 import React, { useState } from "react";
 import { AppBar, Stack, IconButton, Button, Dialog, DialogTitle } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ReflectionSlider from "./ReflectionSlider";
+import { Container } from "@mui/system";
 
 
 
 function TaskListItem({task, active, setActive}) {
   const [opened, setOpened]=useState(false)
+  const [sliderValue, setSliderValue] = React.useState(50);
+  const handleChange = (event, newValue) => {
+    setSliderValue(newValue);
+  };
 	const test = (ev) => {
 		setOpened(false);
 		ev.stopPropagation() // !!!
 	}
 	return <ListItem button={true} onClick={() => setOpened(true)}>
 		<Dialog open={opened} fullScreen={true}>
+            <Container maxWidth='sm'>
 			<AppBar sx={{position: 'relative'}}>
 				<Stack direction="row" spacing={1}>
 					<IconButton onClick={test}>
@@ -26,6 +33,9 @@ function TaskListItem({task, active, setActive}) {
 			<p>
 			{task.description}
 			</p>
+            <p>How did it go?</p>
+            <ReflectionSlider sliderValue={sliderValue} sliderValueChangeHandler={handleChange} />
+            </Container>
 		</Dialog>
 
 		<h2>{task.title}</h2>
