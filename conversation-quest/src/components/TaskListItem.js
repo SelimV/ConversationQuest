c05a4351b281d8e0b20cca20890faf90e5237d28
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2022-11-05 20:52:02 +0200

    qaq

--- conversation-quest/src/components/TaskListItem.js
+++ conversation-quest/src/components/TaskListItem.js
@@ -1,12 +1,41 @@
 import {ListItem } from "@mui/material";
 import React, { useState } from "react";
+import { AppBar, Stack, IconButton, Button, Dialog, DialogTitle } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 
 
-function TaskListItem({task}) {
-    
-    return <ListItem button={true}>
-        {task.name}
+function TaskListItem({task, active, setActive}) {
+  const [opened, setOpened]=useState(false)
+	const test = (ev) => {
+		setOpened(false);
+		ev.stopPropagation() // !!!
+	}
+	return <ListItem button={true} onClick={() => setOpened(true)}>
+		<Dialog open={opened} fullScreen={true}>
+			<AppBar sx={{position: 'relative'}}>
+				<Stack direction="row" spacing={1}>
+					<IconButton onClick={test}>
+						<ArrowBackIcon/>
+					</IconButton>
+					<DialogTitle>
+						{task.title}
+					</DialogTitle>
+				</Stack>
+			</AppBar>
+			<p>
+			{task.description}
+			</p>
+		</Dialog>
+
+		<h2>{task.title}</h2>
+		<br/>
+		{active ?
+			<Button onClick={(ev)=>{setActive(false);ev.stopPropagation();}}>Cancel</Button>
+		:
+			<Button onClick={(ev)=>{setActive(true);ev.stopPropagation();}}>Activate</Button>
+		}
+			
     </ListItem>
 }
 export default TaskListItem;
--- conversation-quest/src/components/TaskListPage.js
+++ conversation-quest/src/components/TaskListPage.js
@@ -1,21 +1,44 @@
 import { Container } from "@mui/system";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TaskList from "./TaskList";
+import Tasks from "../json/tasks.json";
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
 
 function TaskListPage() {
-  const [tasks, setTasks] = useState([{
-    name: "test task",
-    description: "test the task list",
-    completed: false
-  }
-  ])
+  const [activeTasks, setActiveTasks]=useState(JSON.parse(localStorage.getItem("activeTasks") ?? "[]")) //pges: "landing", "login", ...
+	useEffect(() => {
+		localStorage.setItem("activeTasks", JSON.stringify(activeTasks))
+	});
 
-  return <>
-    <div>All/Active selector</div>
-    <div>Filter paneli</div>
-    <Container>
-      <TaskList tasks={tasks} />
-    </Container>
-  </>;
+
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="All" value="1" />
+            <Tab label="Active" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+		  <TaskList tasks={Tasks} activeTasks={activeTasks} setActiveTasks={setActiveTasks} />
+		</TabPanel>
+        <TabPanel value="2">
+			<TaskList tasks={Tasks.filter(task=>activeTasks.includes(task.id))} activeTasks={activeTasks} setActiveTasks={setActiveTasks} />
+		</TabPanel>
+      </TabContext>
+    </Box>
+  );
 }
 export default TaskListPage;
