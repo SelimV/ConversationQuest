1ff636a56440aecaf76d25d18cd77f0657a0005e
Kuoppala Siiri <22868831+siiruli@users.noreply.github.com>
2022-11-06 04:15:34 +0200

    Fix navigation bar

--- conversation-quest/src/components/Navigation.js
+++ conversation-quest/src/components/Navigation.js
@@ -1,22 +1,31 @@
 import React, { useState } from "react";
 import BottomNavigation from '@mui/material/BottomNavigation';
-import { BottomNavigationAction } from "@mui/material";
+import { BottomNavigationAction, Paper } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import Button from "@mui/material/Button";
 
 function NavBar({setPage, initialPage}){
     return <>
-    <BottomNavigation
-      showLabels
-      value={initialPage}
-      onChange={(event, newValue) => {
-		  setPage(newValue)
-      }}
-    >
-      <BottomNavigationAction value="TaskList" label="tasklist" icon={<FavoriteIcon />} />
-      <BottomNavigationAction value="Progress" label="progress" icon={<FavoriteIcon/>} />
-      <BottomNavigationAction value="More" label="more" icon={<FavoriteIcon />} />
-    </BottomNavigation>   
+    <Paper 
+        sx={{
+          position: 'fixed',
+          bottom: 0, 
+          left: 0, 
+          right: 0
+        }} elevation={3}>    
+      <BottomNavigation
+        showLabels
+        value={initialPage}
+        onChange={(event, newValue) => {
+        setPage(newValue)
+        }}
+      >
+        <BottomNavigationAction value="TaskList" label="tasklist" icon={<FavoriteIcon />} />
+        <BottomNavigationAction value="Progress" label="progress" icon={<FavoriteIcon/>} />
+        <BottomNavigationAction value="More" label="more" icon={<FavoriteIcon />} />
+      </BottomNavigation> 
+    </Paper>
+  
   </>;
 }
 export default NavBar;
